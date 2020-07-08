522678e4af5b82886250a0b17b26324ebdbf9b70
megan <meggsnbacon777@gmail.com>
2020-07-08 11:59:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,9 +89,6 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    :yukihiro_matsumoto=>{},
-    programmer_hash.merge(:yukihiro_matsumoto=>{:known_for=>"Ruby"
-      :languages=>["LISP","C"]})
     }
 }
    
